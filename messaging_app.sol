a95bb589b9d95b6e4163a75f5ce31ba88bd36889
Brooke <brookekelseyryan@gmail.com>
2022-03-17 18:33:56 -0700

    documentation
    Added some documentation/ comments.

--- messaging_app.sol
+++ messaging_app.sol
@@ -2,95 +2,152 @@
 
 pragma solidity ^0.8.7;
 
+/** @title Storage stores user information such as public key. */
 contract Storage {
-    mapping(address => string) private keys;
+    mapping(address => string) private keys;        // Hash map, key is user address, string is the public encryption key. 
 
+    /** @dev Stores the user's public encryption key.
+      * @param user_address Memory address of user. 
+      * @param new_key User's public key.
+      */
     function updateKeys(address user_address, string memory new_key) public{
         keys[user_address] = new_key;
     }
 
-    function retrieveMyKey() public view returns (string memory){
+    /** @dev Retreives the user's public encryption key.
+      * @return string Public encryption key for the given user. 
+      */
+    function retrieveMyKey() public view returns (string memory) {
         return keys[msg.sender];
     }
 
-    function retrieveKey(address reciever) public view returns (string memory){
+    /** @dev Retreives the user's public encryption key.
+      * @param reciever Memory address of the user. 
+      * @return string Public encryption key.
+      */
+    function retrieveKey(address reciever) public view returns (string memory) {
          return keys[reciever];
     }
 }
 
+/** @title centralContract Coordinates creating new users and sending messages between users. */
 contract centralContract {
-    address storage_address;
+    address storage_address;    // Memory address of the deployed Storage contract. 
 
+    /** @dev Constructor for the centralContract. 
+      * @param storage_address_ Memory address of deployed storage contract. 
+      */
     constructor (address storage_address_) {
         storage_address = storage_address_;
     }
 
+    /** @dev Creates a new user and stores their public key to the deployed Storage contract. 
+             Alternatively, can call this function from the Sender smart contract.
+      * @param public_key Public encryption key provided by the user. 
+      */
     function create_a_user(string memory public_key) public {
         Storage s = Storage(storage_address);
         s.updateKeys(msg.sender, public_key);
     }
 
-    function get_my_public_key() public view returns (string memory){
+    /** @dev Retreives the user's public encryption key.
+      * @return string User's public encryption key.
+      */
+    function get_my_public_key() public view returns (string memory) {
         Storage s = Storage(storage_address);
         return s.retrieveMyKey();
     }
 
+    /** @dev Sends a message to a given receiver. 
+      * @param reciever Memory address of the receiver.
+      * @param message Message to be sent to the receiver, should be encrypted on the sender's local machine and provided as ciphertext. 
+      */
     function send_message(address reciever, string memory message) public {
-        Reciever r = Reciever(reciever);
+        Reciever r = Reciever(reciever);        // Receiver smart contract 
         r.recieve_message(message);
-        // value = _n;
     }
 
+    /** @dev Retreives the public encryption key of a given receiver. 
+      * @param reciever Memory address of the receiver. 
+      * @return memory Public encryption key.
+      */
     function get_public_key(address reciever) public view returns (string memory){
         Storage s = Storage(storage_address);
         return s.retrieveKey(reciever);
     }
 }
 
+/** @title Sender smart contract. */
 contract Sender {
+
     address smart_contract_address;
-    // uint256 user_id;
     string public_key;
     
+    /** @dev Constructor for the Sender smart contract. 
+      * @param smart_contract_address_ memory address for the central contract. 
+      * @param public_key_ encryption key for the user.  
+      */
     constructor (address smart_contract_address_, string memory public_key_) {
         smart_contract_address = smart_contract_address_;
-        // user_id = user_id_;
         public_key = public_key_;
     }
-    function create_user() public{
+
+    /** @dev Simulates the users asking to create an account. 
+             Alternatively, can call this from the Central Contract. 
+    */
+    function create_user() public {
         centralContract c = centralContract(smart_contract_address);
         c.create_a_user(public_key);
     }
-    function send_message(address reciever, string memory message) public{
+
+    /** @dev Calls the central contract to then send a message to a given receiver.
+      * @param reciever Memory address of the receiver.
+      * @param message Message to be sent. 
+      */
+    function send_message(address reciever, string memory message) public {
         centralContract c = centralContract(smart_contract_address);
         c.send_message(reciever, message);
     }
 
-    function get_public_key(address reciever) public view returns (string memory){
+    /** @dev Retreives the public encryption key from 
+      * @param reciever Memory address of the receiver. 
+      * @return Public key. 
+      */
+    function get_public_key(address reciever) public view returns (string memory) {
         centralContract c = centralContract(smart_contract_address);
         return c.get_public_key(reciever);
     }
 }
 
+/** @title Receiver smart contract that coordinates receiving messages. */
 contract Reciever {
     string message;
     address smart_contract_address;
-    // uint256 user_id;
     string public_key;
     
     constructor (address smart_contract_address_, string memory public_key_) {
         smart_contract_address = smart_contract_address_;
-        // user_id = user_id_;
         public_key = public_key_;
     }
+
+    /** @dev Simulates the users asking to create an account. 
+             Alternatively, can call this from the Central Contract. 
+    */
     function create_user() public{
         centralContract c = centralContract(smart_contract_address);
         c.create_a_user(public_key);
     }
+
+    /** @dev Receives the message. This is typically called from the Sender or Central Contract. 
+      * @param message_ Message for the user. 
+      */
     function recieve_message(string memory message_) public {
         message = message_;
     }
-    function get_message()  public view returns (string memory){
+
+    /** @dev Gets the message associated with the receiver. 
+      */
+    function get_message()  public view returns (string memory) {
         return message;
     }
 }
